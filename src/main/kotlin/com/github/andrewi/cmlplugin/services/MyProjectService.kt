ee1b39c5ed5afbd9e148e9640b15c3bb620326b8
GitHub Action <action@github.com>
2022-08-29 13:40:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/andrewi/cmlplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/andrewi/cmlplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.andrewi.cmlplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andrewi.cmlplugin.MyBundle
 
 class MyProjectService(project: Project) {
 

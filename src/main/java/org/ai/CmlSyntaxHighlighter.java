756870270ee34da3c9a46c4b4a1a2f3337d2a662
asmirnov <asmirnov@solit-clouds.ru>
2022-09-12 17:59:14 +0300

    add meta info, add meta high light

--- src/main/java/org/ai/CmlSyntaxHighlighter.java
+++ src/main/java/org/ai/CmlSyntaxHighlighter.java
@@ -13,8 +13,11 @@ import org.jetbrains.annotations.NotNull;
 import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
 
 public class CmlSyntaxHighlighter implements SyntaxHighlighter {
-  public static final TextAttributesKey LINE_COMMENT =
-      createTextAttributesKey("LINE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+  public static final TextAttributesKey META_START =
+      createTextAttributesKey("META_START", DefaultLanguageHighlighterColors.METADATA);
+
+  public static final TextAttributesKey PROTOCOL =
+      createTextAttributesKey("PROTOCOL", DefaultLanguageHighlighterColors.CONSTANT);
   public static final TextAttributesKey BLOCK_COMMENT =
       createTextAttributesKey("BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
 
@@ -27,18 +30,17 @@ public class CmlSyntaxHighlighter implements SyntaxHighlighter {
   public static final TextAttributesKey DOC =
       createTextAttributesKey("DOC", DefaultLanguageHighlighterColors.STRING);
 
-
-
   public static final TextAttributesKey BAD_CHARACTER =
       createTextAttributesKey("SIMPLE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
 
 
   private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
-  private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{LINE_COMMENT};
   private static final TextAttributesKey[] BLOCK_COMMENT_KEYS = new TextAttributesKey[]{BLOCK_COMMENT};
   private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORD_TOKEN};
   private static final TextAttributesKey[] ID_KEYS = new TextAttributesKey[]{ID};
   private static final TextAttributesKey[] DOC_KEYS = new TextAttributesKey[]{DOC};
+  private static final TextAttributesKey[] META_START_KEYS = new TextAttributesKey[]{META_START};
+  private static final TextAttributesKey[] PROTOCOL_KEYS = new TextAttributesKey[]{PROTOCOL};
   private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
 
 
@@ -49,11 +51,12 @@ public class CmlSyntaxHighlighter implements SyntaxHighlighter {
 
   @Override
   public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
-    if (tokenType.equals(CmlTypes.LINE_COMMENT)) {
-      return COMMENT_KEYS;
-    }
     if (tokenType.equals(CmlTypes.BLOCK_COMMENT)) {
       return BLOCK_COMMENT_KEYS;
+    }
+
+    if (tokenType.equals(CmlTypes.META_START)) {
+      return META_START_KEYS;
     }
 
     if (tokenType.equals(CmlTypes.BOUNDED_CONTEXT_TOKEN) ||
@@ -63,12 +66,24 @@ public class CmlSyntaxHighlighter implements SyntaxHighlighter {
         tokenType.equals(CmlTypes.HINT_TOKEN) ||
         tokenType.equals(CmlTypes.IMPORT_TOKEN) ||
         tokenType.equals(CmlTypes.DOMAIN_VISTION_TOKEN) ||
-        tokenType.equals(CmlTypes.TECHNOLOGY_TOKEN)
+        tokenType.equals(CmlTypes.TECHNOLOGY_TOKEN) ||
+        tokenType.equals(CmlTypes.COMMAND_EVENT_TOKEN) ||
+        tokenType.equals(CmlTypes.DOMAIN_EVENT_TOKEN) ||
+        tokenType.equals(CmlTypes.APPLICATION_TOKEN) ||
+        tokenType.equals(CmlTypes.CAPTION_TOKEN) ||
+        tokenType.equals(CmlTypes.OWNER_TOKEN) ||
+        tokenType.equals(CmlTypes.VIA_TOKEN)
     ) {
       return KEYWORD_KEYS;
     }
 
-    if(tokenType.equals(CmlTypes.STRING)) {
+    if (tokenType.equals(CmlTypes.REST_TOKEN) ||
+        tokenType.equals(CmlTypes.QUEUE_TOKEN)) {
+      return PROTOCOL_KEYS;
+    }
+
+
+    if (tokenType.equals(CmlTypes.STRING)) {
       return DOC_KEYS;
     }
 
